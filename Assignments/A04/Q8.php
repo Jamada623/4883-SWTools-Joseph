0f3b32ed2bfee9b9a98df238de510c63a3903ded
Jamada623 <jamal2501@hotmail.com>
2019-03-01 11:56:11 -0600

    hw

---
+++ Assignments/A04/Q8.php
@@ -0,0 +1,67 @@
+<?php
+//Connect to mysql
+$host = "cs2.mwsu.edu";             // because we are ON the server
+$user = "software_tools";        // user name
+
+// Get username and password from slack
+// The DB username and pass not the ones
+// I sent you to log into the server.
+$password = "horseblanketdonkey";         // password 
+$database = "nfl_data";              // database 
+$mysqli = mysqli_connect($host, $user, $password, $database);
+
+if (mysqli_connect_errno($mysqli)) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+/**
+ * This function runs a SQL query and returns the data in an associative array
+ * that looks like:
+ * $response [
+ *      "success" => true or false
+ *      "error" => contains error if success == false
+ *      "result" => associative array of the result
+ * ]
+ *
+ */
+function runQuery($mysqli,$sql){
+    $response = [];
+
+    // run the query
+    $result = $mysqli->query($sql);
+
+    // If we were successful
+    if($result){
+        $response['success'] = true;
+        // loop through the result printing each row
+        while($row = $result->fetch_assoc()){
+            $response['result'][] = $row;
+        }
+        $result->free();
+    }else{
+        $response['success'] = false;
+        $response['error'] = $mysqli->error;
+    }
+
+    return $response;
+}
+
+// function to find the top 5 players with the shortest average field goal length 
+$sql = "SELECT `playerid`,players.name,players_stats.season,SUM(yards) as rushyards 
+        FROM `players_stats` 
+        LEFT JOIN players on players_stats.playerid = players.id 
+        WHERE rushyards > 40
+        GROUP BY `playerid`
+         ORDER BY rushyards desc LIMIT 5";
+
+$response = runQuery($mysqli, $sql);
+
+echo "<pre>";   // so whitespace matters
+
+
+if($response['success']){
+    foreach($response['result'] as $row){
+        echo "{$row['playerid']} {$row['name']}:{$row['season']}  {$row['rushyards']}: \n";
+    }
+}
---
+++ Assignments/A04/README.md
@@ -0,0 +1,40 @@
+Course: CMPS 4883
+Assignemt: A04
+Date: 3/01/19
+Github username: Jamada623
+Repo url: https://github.com/Jamada623/4883-SWTools-Joseph/tree/master/Assignments
+Name: Jamal Joseph
+
+Project overview: This project uses sql and php to query the data in nfl_data to answer some questions.
+Files : Q1.php
+
+	File description:  This file counts the number of indivdual teams a player played for
+	
+Q2.php
+	File description:  This filee finds the highest total rushing yards by year
+
+Q3.php
+	File description:  This file list the bottom 5 passing players per year
+	
+Q4.php
+	File description:  This file finds the top 5 teams with the most rushes for a loss
+	
+Q5.php
+	File description:  This file finds the team with the most penalties
+	
+Q6.php
+	File description:  This file finds the average number of penalties per year
+	
+Q7.php
+	File description:  This file finds the top 5 players with field goals over 40
+	
+Q8.php
+	File description:  This file finds the top 5 players witht the shortest avg field goal length
+	
+Q9.php
+	File description:  This file ranks the NFL by win/loss
+	
+Q10.php
+	File description:  This file finds the most reoccuring last name
+	
+instructions: upload the php files then go to the path you uploaded the file to and add "filename.php" to the extension to run it
